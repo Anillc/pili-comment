a46a04e7153ef251adb7b7a77ef062a9b38c1676
Anillc <a2488953597@live.cn>
2020-08-07 10:00:35 +0800

    update code

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,6 @@ const path = require('path');
 module.exports = {
 	target: 'node',
 	entry: './src/comment.coffee',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
-		libraryTarget: 'umd'
-	},
 	mode: 'production',
 	module: {
 		rules: [
